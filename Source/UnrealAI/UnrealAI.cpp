a52b24af28c2183c9d0bc6521ccbbc382f76f8f6
Nicolas de gheselle <nicolas@degheselle.fr>
2023-12-27 21:24:25 +0100

    Init

---
+++ Source/UnrealAI/UnrealAI.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealAI, "UnrealAI" );
+
+DEFINE_LOG_CATEGORY(LogUnrealAI)
+ 
