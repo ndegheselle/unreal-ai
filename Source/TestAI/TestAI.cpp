a245e3387ff0e85ff3eade7438d8920b7ced7e40
Nicolas de gheselle <nicolas@degheselle.fr>
2023-12-28 20:19:59 +0100

    Changed project because of weird error

---
+++ Source/TestAI/TestAI.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestAI, "TestAI" );
+
+DEFINE_LOG_CATEGORY(LogTestAI)
+ 
---
+++ Source/TestAI/TestAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestAIGameMode();
+};
+
+
+
